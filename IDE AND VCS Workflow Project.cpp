3f2ed10ffcb0b55c79a222fc253794f61feb4b04
Lam Le <Lam Le@DESKTOP-OQAKGIG>
2025-06-16 14:49:30 -0400

    Add project files.

---
+++ IDE AND VCS Workflow Project.cpp
@@ -0,0 +1,13 @@
+// IDE AND VCS Workflow Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+	char first, middle, last;
+	first = 'L';
+	middle = 'D';
+	last = 'L';
+	cout << first << middle << last << endl;
+	return 0;
+}
