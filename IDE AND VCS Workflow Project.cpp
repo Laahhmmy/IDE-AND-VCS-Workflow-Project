9ae624e1cc3372513358eb0621d47a3e8f9d2329
Lam Le <lamduyle08@gmail.com>
2025-06-23 16:09:18 -0400

    Fixed the error by deleting the first "currentOceanLeave" because I realized that I declared my variable twice.

--- IDE AND VCS Workflow Project.cpp
+++ IDE AND VCS Workflow Project.cpp
@@ -4,16 +4,17 @@
 #include <iostream>
 using namespace std;
 int main() {
-	double currentOceanLevel, fiveYears, sevenYears, tenYears;
+
+	double fiveYears, sevenYears, tenYears;
 	double currentOceanLevel = 0;
 	fiveYears = currentOceanLevel + 1.5 * 5; // 1.5 mm per year for 5 years
 	sevenYears = currentOceanLevel + 1.5 * 7; // 1.5 mm per year for 7 years
 	tenYears = currentOceanLevel + 1.5 * 10; // 1.5 mm per year for 10 years
-	cout << "The current ocean level is" << currentOceanLevel << " mm." << endl;
+	cout << "The current ocean level is " << currentOceanLevel << " mm." << endl;
 	cout << "The ocean level in 5 years will be " << fiveYears << " mm." << endl;
 	cout << "The ocean level in 7 years will be " << sevenYears << " mm." << endl;
 	cout << "The ocean level in 10 years will be " << tenYears << " mm." << endl;
 
-
+	return 0;
 
 }
