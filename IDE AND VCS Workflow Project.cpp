235e3ff812aab1b12126737b16ddc08edbc4eda6
Lam Le <lamduyle08@gmail.com>
2025-06-23 17:36:42 -0400

    Fixed the error by adding values to the variables.

--- IDE AND VCS Workflow Project.cpp
+++ IDE AND VCS Workflow Project.cpp
@@ -6,10 +6,13 @@ using namespace std;
 int main() {
 	double customers, percentoneormore, percentcitrus;
 	double oneormore;
-	oneormore = customers * percentoneormore / 100.0;
+	customers = 16500; // Total number of customers surveyed
+	percentoneormore = 15.0; // Percentage of customers purchasing one or more energy drinks per week
+	percentcitrus = 58.0; // Percentage of those purchasing one or more energy drinks that prefer citrus flavored drinks
+	oneormore = customers * percentoneormore / 100.0; 
 	double citrus;
 	citrus = oneormore * percentcitrus / 100.0;
-	cout << "The approximate number of customers in the survey purchasing one or more energy drinks per week is " << oneormore << endl;
-	cout << "The approximate number of customers in the survey purchasing citrus flavored energy drinks per week is " << citrus << endl;
+	cout << "The approximate number of customers in the survey purchasing one or more energy drinks per week is " << oneormore << "." << endl;
+	cout << "The approximate number of customers in the survey purchasing citrus flavored energy drinks per week is " << citrus << "." << endl;
 	return 0;
 }
