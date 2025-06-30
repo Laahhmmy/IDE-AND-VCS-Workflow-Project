85186070b4f24b5d6be13fba37820f120e3efb93
Lam Le <lamduyle08@gmail.com>
2025-06-30 15:35:53 -0400

    Created the program and it works, but still need to add a few things.

--- IDE AND VCS Workflow Project.cpp
+++ IDE AND VCS Workflow Project.cpp
@@ -2,12 +2,29 @@
 //
 
 #include <iostream>
+#include <iomanip>
+#include <cmath>
 using namespace std;
 int main() {
-	char first, middle, last;
-	first = 'L';
-	middle = 'D';
-	last = 'L';
-	cout << first << middle << last << endl;
+	double principal, rate;
+	double timesCompounded;
+	cout << "What is the balance in your savings account? ";
+	cin >> principal;
+	cout << "What is the annual interest rate (in percent)? ";
+	cin >> rate;
+	cout << "How many times is the interest compounded per year? ";
+	cin >> timesCompounded;
+
+	rate = rate / 100; // Convert percentage to decimal
+	double balance = principal * pow(1 + (rate / timesCompounded), timesCompounded);
+	double interest = balance - principal;
+
+	cout << fixed << setprecision(2); // Set output to 2 decimal places
+	cout << "Interest Rate: " << rate * 100 << "%" << endl;
+	cout << "Times Compounded: " << timesCompounded << endl;
+	cout << "Principal: $" << principal << endl;
+	cout << "Interest: $" << interest << endl;
+	cout << "Amount in savings: $" << balance << endl;
+
 	return 0;
 }
