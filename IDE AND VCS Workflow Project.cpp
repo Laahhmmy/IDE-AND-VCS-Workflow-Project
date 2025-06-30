cf4f75a94095954e10b85430034ea2ad27ef764a
Lam Le <lamduyle08@gmail.com>
2025-06-30 13:34:06 -0400

    Created program to display and calculate the amount of sugar, butter, and flour needed to make a certain number of cookies.

--- IDE AND VCS Workflow Project.cpp
+++ IDE AND VCS Workflow Project.cpp
@@ -1,13 +1,32 @@
-// IDE AND VCS Workflow Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <iostream>  
+#include <iomanip>  
 
-#include <iostream>
-using namespace std;
-int main() {
-	char first, middle, last;
-	first = 'L';
-	middle = 'D';
-	last = 'L';
-	cout << first << middle << last << endl;
+using namespace std;  
+
+int main() {  
+	const double sugarCups = 1.5; // cups of sugar per batch  
+	const double butterCups = 1.0; // cups of butter per batch  
+	const double flourCups = 2.75; // cups of flour per batch  
+
+	const double batchYield = 48; // cookies per batch  
+
+	double cookiesWanted; // Declare the variable cookiesWanted  
+	double totalSugarCups;
+	double totalButterCups;
+	double totalFlourCups;
+
+	cout << "How many cookies do you want to make? ";  
+	cin >> cookiesWanted;  
+
+	totalSugarCups = (cookiesWanted / batchYield) * sugarCups;
+	totalButterCups = (cookiesWanted / batchYield) * butterCups;
+	totalFlourCups = (cookiesWanted / batchYield) * flourCups;
+	cout << fixed << setprecision(2); // Set precision for output
+	cout << "You will need:\n";
+	cout << totalSugarCups << " cups of sugar\n";
+	cout << totalButterCups << " cups of butter\n";
+	cout << totalFlourCups << " cups of flour\n";
 	return 0;
+
+	
 }
