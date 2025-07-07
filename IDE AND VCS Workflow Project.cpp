484209b6a916cb030fa47d1451957b37942f615a
Lam Le <lamduyle08@gmail.com>
2025-07-07 16:01:49 -0400

    Created program and about to test

--- IDE AND VCS Workflow Project.cpp
+++ IDE AND VCS Workflow Project.cpp
@@ -4,10 +4,52 @@
 #include <iostream>
 using namespace std;
 int main() {
-	char first, middle, last;
-	first = 'L';
-	middle = 'D';
-	last = 'L';
-	cout << first << middle << last << endl;
-	return 0;
+	
+	cout << "Geometry Calculator\n";
+	cout << "1. Calculate area of a Circle \n";
+	cout << "2. Calculate area of a Rectangle \n";
+	cout << "3. Calculate area of a Triangle \n";
+	cout << "4. Exit \n";
+	cout << "Please select an option (1-4): ";
+
+	int choice;
+	cin >> choice;
+	switch (choice) {
+		case 1: {
+			// Area of Circle
+			const double PI = 3.14159;
+			double radius;
+			cout << "Enter the radius of the circle: ";
+			cin >> radius;
+			double areaCircle = PI * radius * radius;
+			cout << "The area of the circle is: " << areaCircle << endl;
+			break;
+		}
+		case 2: {
+			// Area of Rectangle
+			double length, width;
+			cout << "Enter the length of the rectangle: ";
+			cin >> length;
+			cout << "Enter the width of the rectangle: ";
+			cin >> width;
+			double areaRectangle = length * width;
+			cout << "The area of the rectangle is: " << areaRectangle << endl;
+			break;
+		}
+		case 3: {
+			// Area of Triangle
+			double base, height;
+			cout << "Enter the base of the triangle: ";
+			cin >> base;
+			cout << "Enter the height of the triangle: ";
+			cin >> height;
+			double areaTriangle = 0.5 * base * height;
+			cout << "The area of the triangle is: " << areaTriangle << endl;
+			break;
+		}
+		case 4:
+			cout << "Exiting program." << endl;
+			break;
+		default:
+			cout << "Invalid choice. Please select a valid option (1-4)." << endl;
 }
