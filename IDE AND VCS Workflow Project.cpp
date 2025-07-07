56cd5a24dd973db4b7246c05356bbd3df72de2d2
Lam Le <lamduyle08@gmail.com>
2025-07-07 15:37:25 -0400

    Created program and about to test

--- IDE AND VCS Workflow Project.cpp
+++ IDE AND VCS Workflow Project.cpp
@@ -2,12 +2,36 @@
 //
 
 #include <iostream>
+#include <iomanip> // For std::setprecision and std::showpoint
 using namespace std;
 int main() {
-	char first, middle, last;
-	first = 'L';
-	middle = 'D';
-	last = 'L';
-	cout << first << middle << last << endl;
+	cout << "What is the weight of the package in kilograms? ";
+	double weight;
+	cin >> weight;
+
+	if (weight <= 0) {
+		cout << "Invalid weight. Please enter a non-negative value." << endl;
+		return 1; // Exit with an error code
+	} else if (weight > 20) {
+		cout << "Invalid weight. The maximum weight for a package is 20 kilograms." << endl;
+		return 1; // Exit with an error code
+	}
+	cout << "What is the distance to be shipped in kilometers? ";
+	double distance;
+	cin >> distance;
+	if (distance < 10 || distance > 3000) {
+		cout << "Invalid distance. The company's minumun and maximum shipping distances are between 10 and 3000 kilometers." << endl;
+		return 1; // Exit with an error code
+	}
+	cout << fixed << setprecision(2) << showpoint;
+	if (weight <= 2) {
+		cout << "It will cost $" << distance / 500 * 1.10;
+	} else if (weight > 2 || weight <= 6) {
+		cout << "It will cost $" << distance / 500 * 2.20;
+	} else if (weight >6 || weight <= 10) {
+		cout << "It will cost $" << distance / 500 * 3.70;
+	} else if (weight > 10 || weight <= 20) {
+		cout << "It will cost $" << distance / 500 * 4.80;
+	}
 	return 0;
 }
