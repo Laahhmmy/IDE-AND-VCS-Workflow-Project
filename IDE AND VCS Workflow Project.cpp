e5819b5493290d1a5ad99e59dd9397fe322d7a3d
Lam Le <lamduyle08@gmail.com>
2025-07-15 22:00:43 -0400

    created code

--- IDE AND VCS Workflow Project.cpp
+++ IDE AND VCS Workflow Project.cpp
@@ -2,12 +2,29 @@
 //
 
 #include <iostream>
+#include <random>
 using namespace std;
 int main() {
-	char first, middle, last;
-	first = 'L';
-	middle = 'D';
-	last = 'L';
-	cout << first << middle << last << endl;
-	return 0;
+	int randomNumber;
+	int number;
+	random_device rd;
+	mt19937 gen(rd()); // Initialize random number generator with a random seed
+	uniform_int_distribution<> dist(0, 100);
+	cout << "Guess a random number: ";
+	cin >> number;
+
+
+	if (number == randomNumber) {
+		cout << "Congrats you guessed the correct number";
+	} 
+	else if (number > randomNumber) {
+		cout << "Too high, try again.";
+		}
+	else if (number < randomNumber) {
+		cout << "Too low, try again.";
+		}
+	else {
+		cout << "Invalid input, please enter a number between 0 and 100.";
+	}
+
 }
