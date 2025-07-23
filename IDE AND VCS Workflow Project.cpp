4a2ef7563a3feaa095da730916319d584ed0a19b
Lam Le <lamduyle08@gmail.com>
2025-07-22 20:52:17 -0400

    created foundation for code

--- IDE AND VCS Workflow Project.cpp
+++ IDE AND VCS Workflow Project.cpp
@@ -2,12 +2,33 @@
 //
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+
+void getLength_Width(double& length, double& width) {
+	cout << "Enter the length: ";
+	cin >> length;
+	cout << "Enter the width: ";
+	cin >> width;
+}
+void calculateArea(double length, double width, double& area) {
+	area = length * width;
+}
+void calcPerimeter(double length, double width, double& perimeter) {
+	perimeter = 2 * (length + width);
+}
+void displayProperties(double length, double width, double area, double perimeter) {
+	cout << fixed << setprecision(2);
+	cout << "Area: " << area << endl;
+	cout << "Perimeter: " << perimeter << endl;
+}
+
 int main() {
-	char first, middle, last;
-	first = 'L';
-	middle = 'D';
-	last = 'L';
-	cout << first << middle << last << endl;
+	double length, width, area, perimeter;
+	getLength_Width(length, width);
+	calculateArea(length, width, area);
+	calcPerimeter(length, width, perimeter);
+	displayProperties(length, width, area, perimeter);
 	return 0;
 }
