2321672676352a011daf020bbb0e029c0f3b35fc
Lam Le <lamduyle08@gmail.com>
2025-08-04 18:47:13 -0400

    created initialiize board function

--- IDE AND VCS Workflow Project.cpp
+++ IDE AND VCS Workflow Project.cpp
@@ -16,3 +16,9 @@ string checkForWinner(char board[3][3], char token);
 int main() {
    
 }
+
+void initializeBoard(char board[3][3]) {
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            board[i][j] = '*';
+}
